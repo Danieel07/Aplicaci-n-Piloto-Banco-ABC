3922703da5b7d513c2835769f6fa2f901bd04909
Daniel Nieto Llanos <dnietollanos@gmail.com>
2024-08-01 14:55:34 -0500

    Inicio Logica Clase  ClienteDAO

--- src/co/edu/unbosque/model/persistence/TransaccionDAO.java
+++ src/co/edu/unbosque/model/persistence/TransaccionDAO.java
@@ -1,5 +1,15 @@
 package co.edu.unbosque.model.persistence;
 
-public class TransaccionDAO {
+import java.util.ArrayList;
 
+import co.edu.unbosque.model.Transaccion;
+
+public class TransaccionDAO {
+	private ArrayList<Transaccion> lista;
+	private ClienteDAO clienteDAO;
+	
+	public TransaccionDAO() {
+		lista = new ArrayList<>();
+		clienteDAO =  new ClienteDAO();
+	}
 }
